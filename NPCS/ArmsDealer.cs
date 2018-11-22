e82a98ba08571f5cf8b01e69447124def4e3f6bd
valkyrienyanko <sebastianbelle074@gmail.com>
2018-11-21 18:28:51 -0600

    party fix + balance

--- NPCS/ArmsDealer.cs
+++ NPCS/ArmsDealer.cs
@@ -82,68 +82,67 @@ namespace MerchantsPlus.NPCS
                 if (!Main.hardMode) { // If not in hardmode.
                     if (NPC.downedSlimeKing) // If the slime king was killed.
                     {
-                        shop.item[0].SetDefaults(ItemID.SilverBullet); // Replace default bullets with silver bullets
+                        
                         shop.item[1].SetDefaults(ItemID.TheUndertaker); // Replace default flintlock pistol with the undertaker
                         shop.item[nextSlot++].SetDefaults(ItemID.RedRyder); // Add red ryder as an additional item to the shop
                     }
 
                     if (NPC.downedBoss1) // If the eye of cthulu was killed
                     {
-                        shop.item[0].SetDefaults(ItemID.MeteorShot); // Replace silver bullets with meteor bullets
+                        shop.item[0].SetDefaults(ItemID.SilverBullet);
                         shop.item[1].SetDefaults(ItemID.Revolver); // Replace undertaker with revolver
                         shop.item[nextSlot++].SetDefaults(ItemID.Boomstick); // Add boom stick as an additional item to the shop
                     }
 
                     if (NPC.downedBoss2) // If the eater of worlds or the brain of cthulu was killed
                     {
-                        shop.item[0].SetDefaults(ItemID.GoldenBullet);
+                        shop.item[0].SetDefaults(ItemID.MeteorShot);
                         shop.item[1].SetDefaults(ItemID.Handgun); // Replace revolver with handgun
                         shop.item[nextSlot++].SetDefaults(ItemID.Musket); // Add musket as an additional item to the shop
                     }
 
                     if (NPC.downedQueenBee) // If the queen bee was killed
                     {
-                        shop.item[0].SetDefaults(ItemID.NanoBullet);
                         shop.item[1].SetDefaults(ItemID.PhoenixBlaster); // Replace handgun with phoenix blaster
                     }
 
                     if (NPC.downedBoss3) // If skeletron was killed
                     {
-                        shop.item[0].SetDefaults(ItemID.ExplodingBullet); // Replace golden bullets with exploding bullets
                         shop.item[nextSlot++].SetDefaults(ItemID.ClockworkAssaultRifle); // Add a rifle
                         shop.item[nextSlot++].SetDefaults(ItemID.OnyxBlaster); // Add a rifle
                     }
                 }
 
                 if (Main.hardMode) { // If in hardmode
-                    shop.item[0].SetDefaults(ItemID.CrystalBullet);
-                    shop.item[1].SetDefaults(ItemID.OnyxBlaster); // Replace silver bullets with this
-                    shop.item[2].SetDefaults(ItemID.HellfireArrow); // Replace unholy arrows with this
+                    shop.item[0].SetDefaults(ItemID.MeteorShot);
+                    shop.item[1].SetDefaults(ItemID.GoldenBullet);
                     shop.item[3].SetDefaults(ItemID.Uzi); // Pistol slot
                     shop.item[4].SetDefaults(ItemID.ClockworkAssaultRifle); // Replace with rifle
+                    shop.item[nextSlot++].SetDefaults(ItemID.OnyxBlaster);
 
                     if (NPC.downedMechBossAny) // If any mech boss was killed
                     {
-                        shop.item[0].SetDefaults(ItemID.HighVelocityBullet);
-                        shop.item[2].SetDefaults(ItemID.HolyArrow);
                         shop.item[nextSlot++].SetDefaults(ItemID.Gatligator);
                     }
 
                     if (NPC.downedMechBoss1)
                     {
-                        shop.item[0].SetDefaults(ItemID.IchorBullet);
-                        shop.item[2].SetDefaults(ItemID.IchorArrow);
+                        shop.item[0].SetDefaults(ItemID.CursedBullet);
+                        shop.item[1].SetDefaults(ItemID.NanoBullet);
+                        shop.item[2].SetDefaults(ItemID.CursedArrow);
                     }
 
                     if (NPC.downedMechBoss2)
                     {
-                        shop.item[0].SetDefaults(ItemID.CursedBullet);
-                        shop.item[2].SetDefaults(ItemID.CursedArrow);
+                        shop.item[0].SetDefaults(ItemID.IchorBullet);
+                        shop.item[1].SetDefaults(ItemID.HighVelocityBullet);
+                        shop.item[2].SetDefaults(ItemID.IchorArrow);
                     }
 
                     if (NPC.downedMechBoss1 && NPC.downedMechBoss2 && NPC.downedMechBoss3)
                     {
-                        shop.item[0].SetDefaults(ItemID.VenomBullet);
+                        shop.item[0].SetDefaults(ItemID.CrystalBullet);
+                        shop.item[1].SetDefaults(ItemID.VenomBullet);
                         shop.item[2].SetDefaults(ItemID.VenomArrow);
                         shop.item[3].SetDefaults(ItemID.VenusMagnum);
                         shop.item[4].SetDefaults(ItemID.Megashark);
@@ -163,20 +162,37 @@ namespace MerchantsPlus.NPCS
 
                     if (NPC.downedGolemBoss)
                     {
-                        shop.item[4].SetDefaults(ItemID.SDMG);
+                        
                     }
 
                     if (NPC.downedFishron)
                     {
                         shop.item[nextSlot++].SetDefaults(ItemID.CandyCornRifle);
+                    }
+
+                    if (NPC.downedMoonlord) {
                         shop.item[0].SetDefaults(ItemID.MoonlordBullet);
                         shop.item[2].SetDefaults(ItemID.MoonlordArrow);
+                        shop.item[nextSlot++].SetDefaults(ItemID.SDMG);
                     }
                 }
 
                 // If the dryad is in the town and is wearing a party hat than the arms dealer will sell party bullets.
-                if (Main.npc[NPCID.Dryad].UsesPartyHat()) {
-                    shop.item[0].SetDefaults(ItemID.PartyBullet);
+                short[] npcs = {NPCID.ArmsDealer, NPCID.Clothier, NPCID.Cyborg, NPCID.Demolitionist, NPCID.Dryad, NPCID.DyeTrader, NPCID.GoblinTinkerer, NPCID.Mechanic, NPCID.Painter, NPCID.PartyGirl, NPCID.Pirate, NPCID.SantaClaus, NPCID.Steampunker, NPCID.Stylist, NPCID.DD2Bartender, NPCID.Truffle, NPCID.WitchDoctor, NPCID.Wizard};
+
+                foreach (short npc in npcs) {
+                    if (Main.npc[npc].UsesPartyHat())
+                    {
+                        if (Main.hardMode)
+                        {
+                            shop.item[1].SetDefaults(ItemID.PartyBullet);
+                        }
+                        else {
+                            shop.item[nextSlot++].SetDefaults(ItemID.PartyBullet);
+                        }
+                        
+                        break;
+                    }
                 }
             }
         }
