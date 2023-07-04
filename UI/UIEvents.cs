6878191262e2d607f95c178a55b037040733fe76
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-07-03 22:41:46 -0500

    Lots of fixes

--- UI/UIEvents.cs
+++ UI/UIEvents.cs
@@ -4,19 +4,64 @@ namespace MerchantsPlus.UI;
 
 internal class UIEvents : GlobalNPC
 {
+    static NPC currentNPC;
+
     public override bool? CanChat(NPC npc)
     {
-        if (ShopUI.Visible || !Utils.TalkingToNPC())
+        // Do not change shop if the player is talking to the same NPC
+        // Do not change shop if not talking to a NPC
+        // Do not change shop if this is not a shop NPC
+        if (npc == currentNPC || !Utils.TalkingToNPC() || !IsShopNPC(npc))
             return base.CanChat(npc);
 
+        // Player must close the current shop before opening up another
+        if (ShopUI.Visible)
+            return base.CanChat(npc);
+
+        currentNPC = npc;
         SetShopIndex(npc);
-        Main.NewText("Showing shop ui");
+        Main.NewText("Showing shop ui for " + npc.FullName);
         ModContent.GetInstance<ModifyUI>().ShowShopUI();
 
         // Reset shop category index
         ShopUI.ShopCycleIndexes[ShopUI.CurrentShopIndex] = 0;
 
         return base.CanChat(npc);
+    }
+
+    static bool IsShopNPC(NPC npc)
+    {
+        switch (npc.type)
+        {
+            case NPCID.Angler:
+            case NPCID.ArmsDealer:
+            case NPCID.Clothier:
+            case NPCID.Cyborg:
+            case NPCID.Demolitionist:
+            case NPCID.Dryad:
+            case NPCID.DyeTrader:
+            case NPCID.GoblinTinkerer:
+            case NPCID.Guide:
+            case NPCID.Mechanic:
+            case NPCID.Merchant:
+            case NPCID.Nurse:
+            case NPCID.Painter:
+            case NPCID.PartyGirl:
+            case NPCID.Pirate:
+            case NPCID.SantaClaus:
+            case NPCID.SkeletonMerchant:
+            case NPCID.Steampunker:
+            case NPCID.Stylist:
+            case NPCID.DD2Bartender:
+            case NPCID.TaxCollector:
+            case NPCID.TravellingMerchant:
+            case NPCID.Truffle:
+            case NPCID.WitchDoctor:
+            case NPCID.Wizard:
+                return true;
+        }
+
+        return false;
     }
 
     static void SetShopIndex(NPC npc)
