7a055df68640f6dde2fd78572d05b65de226bd2f
Tom Pohorily <sebastianbelle074@gmail.com>
2026-03-24 19:09:59 -0500

    Convert majority of shops to expanded

--- MerchantsPlus/Shops/Catalogs/ShopTaxCollectorCatalog.cs
+++
@@ -1,8 +0,0 @@
-namespace MerchantsPlus.Shops;
-
-public static class ShopTaxCollectorCatalog
-{
-    public static readonly string[] ShopNames = [];
-    public const int TaxCollectorExclusiveItemId = ItemID.UglySweater;
-    public static readonly int TaxCollectorExclusiveItemPrice = Coins.Platinum(99);
-}
--- MerchantsPlus/Shops/ShopSection.cs
+++
@@ -1,3 +0,0 @@
-namespace MerchantsPlus.Shops;
-
-public sealed record ShopSection(int[] ItemIds, int? Price = null);
